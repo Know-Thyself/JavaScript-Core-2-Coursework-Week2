3eb10b5c70119b7e3b44097d6d1c31988817a86b
Know-Thyself <67852769+Know-Thyself@users.noreply.github.com>
2021-01-22 19:34:48 +0000

    Project 2-todo list and bike-project part 2 are completed
    N.B: The advanced part of the todo list isn't done yet. I will still be working on that.

--- projects/1-bike-project/js/main.js
+++ projects/1-bike-project/js/main.js
@@ -1,5 +1,6 @@
 // Variables are declared and defined to access and manipulate The DOM.
 
+// Part One
 let blue = document.querySelector('#blueBtn');
 let orange = document.querySelector('#orangeBtn');
 let green = document.getElementById('greenBtn');
@@ -35,3 +36,30 @@ let whenGreenIsClicked = function() {
 blue.addEventListener('click', whenBlueIsClicked);
 orange.addEventListener('click', whenOrangeIsClicked);
 green.addEventListener('click', whenGreenIsClicked);
+
+// Part Two
+// Selecting and assigning variables for the elements to be targeted by the event listener. 
+let buttons = document.getElementsByTagName('button');
+let button = buttons[4];
+let emailInput = document.querySelector('#exampleInputEmail1'); 
+let nameInput = document.querySelector('#example-text-input');
+let textArea = document.querySelector('#exampleTextarea'); 
+let regEx = /\S+@\S+\.\S+/;
+
+// An event listener with an anonymous function to check the required form validation criteria.
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+   if (emailInput.value !== '' && regEx.test(emailInput.value) === true && nameInput.value !== '' && textArea.value.length > 0 ) {
+       alert ('Thank you for filling out the form!');
+       emailInput.value = '';
+       textArea.value = '';
+       nameInput.value = '';
+   } else if (nameInput.value === '') {
+       nameInput.style.backgroundColor = 'red';
+     
+     } else if (textArea.value.length === 0) {
+       textArea.style.backgroundColor = 'red';
+       } else if (emailInput.value === '' || regEx.test(emailInput.value) === false) {
+       emailInput.style.backgroundColor = 'red';
+         } 
+});
--- projects/2-todo-list/script.js
+++ projects/2-todo-list/script.js
@@ -1,6 +1,54 @@
+
+
 function populateTodoList(todos) {
-  let list = document.getElementById("todo-list");
+  // let list = document.getElementById("todo-list");
   // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
+  // Selecting ul first
+  let ul = document.querySelector('#todo-list');
+
+  // Mapping the todos object to create a new array consisting of tasks.
+  let todoList = todos.map(elem => elem.task);
+  
+  // Iterating through each task item to create the exact number of li elements we need along with check and trash icons for the span tag.
+  todoList.forEach(function(item) {
+    let li = document.createElement('li');
+    let text = document.createTextNode(item);
+    li.appendChild(text);
+    li.classList.add('list-group-item');
+    
+    // Creating icons
+    let check = document.createElement('i');
+    check.classList.add('fa', 'fa-check');
+    let trash = document.createElement('i');
+    trash.classList.add('fa','fa-trash');
+    let span = document.createElement('span');
+    span.classList.add('badge', 'bg-primary', 'rounded-pill');
+    
+    // Appending the elements created above to where they belong
+    span.appendChild(check);
+    span.appendChild(trash);
+    li.appendChild(span);
+    ul.appendChild(li);
+
+    // A bit of styling
+    li.style.display = 'flex';
+    li.style.justifyContent = 'space-between';
+    li.style.alignItems = 'center';
+    
+    // Event listener for the check button to line through when clicked.
+    check.addEventListener('click', (e) => {
+      e.preventDefault();
+      li.style.textDecoration = 'line-through';
+
+    })
+    // An event listener to delete a todo list when trash icon is clicked.
+    trash.onclick = (e) => {
+      e.preventDefault();
+      li.remove();
+    }
+
+  })
+
 }
 
 // These are the same todos that currently display in the HTML
@@ -13,13 +61,64 @@ let todos = [
 populateTodoList(todos);
 
 // This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
-function addNewTodo(event) {
+// I had to comment the initial function out as it was interfering with the rest of my code and creating duplicated empty lists when I add a new todo list.
+// function addNewTodo(event) {
   // The code below prevents the page from refreshing when we click the 'Add Todo' button.
-  event.preventDefault();
+  // event.preventDefault();
   // Write your code here... and remember to reset the input field to be blank after creating a todo!
-}
 
-// Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
+
+  // The button element is selected and a variable is defined for it.
+  let addButton = document.querySelector('.btn');
+  
+  // An event listener for the button
+  addButton.addEventListener('click', (e) => {
+    // The code below prevents the page from refreshing when we click the 'Add Todo' button.
+    e.preventDefault();
+    // The input is selected and a new text node is created for a new list to be added
+    let input = document.querySelector('#todoInput');
+    let newTodo = document.createTextNode(input.value);
+    
+    // A new list element is created along with with new check, trash and span. This is where a new list goes when it is added.
+    let li = document.createElement('li');
+    li.appendChild(newTodo);
+    li.classList.add('list-group-item');
+    let check = document.createElement('i');
+    check.classList.add('fa', 'fa-check');
+    let trash = document.createElement('i');
+    trash.classList.add('fa','fa-trash');
+    let span = document.createElement('span');
+    span.classList.add('badge', 'bg-primary', 'rounded-pill');
+
+    // An event listener for the check icon to line through when it is clicked.
+    check.addEventListener('click', (e) => {
+      e.preventDefault();
+      li.style.textDecoration = 'line-through';
+    })
+    // An other event listener for the trash icon to delete a list when clicked.
+    trash.onclick = (e) => {
+      e.preventDefault();
+      li.remove();
+
+    }
+    // The original ul is selected and the new li is appended to it in.
+    let ul = document.querySelector('#todo-list');
+    span.appendChild(check);
+    span.appendChild(trash);
+    li.appendChild(span);
+    ul.appendChild(li);
+  // A bit of styling so that everything appears the way it should.
+  li.style.display = 'flex';
+  li.style.justifyContent = 'space-between';
+  li.style.alignItems = 'center';
+
+  input.value = ''; 
+
+  })
+// }
+
+// Advanced challenge: Write a function that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
 function deleteAllCompletedTodos() {
   // Write your code here...
+  
 }
